4d61a5927c73b0a3f9265587bc98839d44e14c94
Oyindamola <damolaoyin005@gmail.com>
2026-02-13 18:45:46 +0100

    wip

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,13 @@ const item = [
     {name: "Chicken", price: 4000},
     {name: "Salad", price : 1000},
 ];
-for(let i=0; i<item.lenght; i++)
+let grandtotal = 0'
+for(let i=0; i<item.length; i++)
 {
 let quantity = prompt("How many" + item[i].name + "do you want to order");
 quantity =Number(quantity);
 let totalCost = quantity * item[i].price;
-alert("Total order for" + item[i].name + "is #" + totalCost);
+
+grandtotal += totalCost;
 };
+alert ("grand total for your oder is #" + grandtotal)
