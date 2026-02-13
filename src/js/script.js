05e832abd3a53c7506a8b784eef1908c41ea3003
Oyindamola <damolaoyin005@gmail.com>
2026-02-13 18:19:56 +0100

    wip

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 const menu = [
-    {name: "Jollof Rice", price: 2000};
-    {name: "Chicken", price: 4000};
-    {name: "Salad", price: 1000};
-]
+    {name: "Jollof Rice", price: 2000},
+    {name: "Chicken", price: 4000},
+    {name: "Salad", price : 1000},
+];
 for(let i=0; i<menubar.lenght; i++);
 
 let quantity = prompt("How many portion" + menu[i].name + "do you want to order");
 quantity =number(quantity);
 let totalCost = quantity * menu[i].price;
-alert("Total order placed for" + menu[i])
+alert("Total order placed for" + menu[i]);
